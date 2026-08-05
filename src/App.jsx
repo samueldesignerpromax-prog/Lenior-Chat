1ab6e3751ef40b14cac383f40dbfab4b6d04c34f
Samuel Tech IA <samueldesignerpromax@gmail.com>
2026-08-05 17:05:59 -0300

    Remove BrowserRouter and Routes from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -7,17 +6,14 @@ import Chat from './pages/Chat';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Toaster position="top-right" toastOptions={{ style: { background: '#1a1a1a', color: '#fff', border: '1px solid #333' } }} />
       <Navbar />
       <main className="container" style={{ flex: 1, paddingTop: '30px', paddingBottom: '60px', minHeight: '80vh' }}>
-        <Routes>
-          <Route path="/" element={<Chat />} />
-          <Route path="*" element={<Chat />} />
-        </Routes>
+        <Chat />
       </main>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
